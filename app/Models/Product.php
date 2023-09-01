0b4301cf08c6606796b2d2bf2f31940375afe175
Omer Fayyaz <omerfayyaz.engr@gmail.com>
2023-09-02 01:27:40 +0500

    Creating new Category Table and adding relation to Products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'price', 'status'];
+    protected $fillable = ['name', 'price', 'status', 'category_id'];
+
+    public function category (): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 
 }
