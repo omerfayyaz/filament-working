58d3d22e6ab6c3a4d68f48d6dbfb2a1331ed6c17
Omer Fayyaz <omerfayyaz.engr@gmail.com>
2023-09-04 14:00:01 +0500

    tags relationships in Product fix

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -20,7 +20,7 @@ class Product extends Model
 
     public function tags (): BelongsToMany
     {
-        return $this->belongsToMany(Tags::class);
+        return $this->belongsToMany(Tag::class);
     }
 
 }
